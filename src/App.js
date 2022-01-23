5d4404e05952818c900588696fb52d3548ba10f5
mistorious <kimtm0818@gmail.com>
2022-01-23 21:34:21 +0900

    first Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from '@mui/material';
+import React from "react";
+import CssBaseline from '@mui/material/CssBaseline';
+import Main from "./Main"
+
+function App() {
+  return (
+    <div className="App">
+      <CssBaseline />
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Main.js
@@ -0,0 +1,12 @@
+function Main () {
+
+    return (
+        <div className="main-page-container">
+            <img src="backgrounds/background1.jpg" width={"100%"} height={"100%"}/>
+            <div className="main-page-text">How are you?</div>
+        </div>
+    )
+
+}
+
+export default Main;
